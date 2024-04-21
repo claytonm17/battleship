56ba9bac2f056297dc88e95c706d5639396eaa1c
claytonm17 <133607487+claytonm17@users.noreply.github.com>
2024-04-21 19:18:26 -0400

    Test game control for players
    Abstracted creating ships on grid

--- src/__tests__/player.test.ts
+++ src/__tests__/player.test.ts
@@ -63,3 +63,138 @@ test("Be able to attack the other player's board", () => {
     )
     expect(carrierP1.health).toBe(4);
 });
+
+test("Be able to attack the other player's board", () => {
+    const player1 = new Player("p1");
+    const player2 = new Player("p2");
+    const carrierP1 = new Ship(1, 5, [6,2], "V");
+    player2.gameboard.placeShip(carrierP1);
+    player1.attack([3,1], player2);
+    player1.attack([6,2], player2);
+    player1.attack([6,3], player2);
+    player1.attack([6,4], player2);
+    player1.attack([6,5], player2);
+    player1.attack([6,6], player2);
+    expect(player2.gameboard.board).toStrictEqual(
+        [
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, -1, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, -1, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, -1, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, -1, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, -1, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, -1, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        ]
+    )
+    expect(carrierP1.health).toBe(0);
+    expect(player2.gameboard.checkShipsSunk()).toBe(true);
+});
+
+test("Be able to attack the other player's board", () => {
+    const player1 = new Player("p1");
+    const player2 = new Player("p2");
+    const carrierP1 = new Ship(1, 5, [6,2], "V");
+    const destroyerP1 = new Ship(5, 2, [1,2], "H");
+    player2.gameboard.placeShip(carrierP1);
+    player2.gameboard.placeShip(destroyerP1);
+    player1.attack([3,1], player2);
+    player1.attack([6,2], player2);
+    player1.attack([6,3], player2);
+    player1.attack([6,4], player2);
+    player1.attack([6,5], player2);
+    player1.attack([6,6], player2);
+    expect(player2.gameboard.board).toStrictEqual(
+        [
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, -1, 0, 0, 0, 0, 0, 0],
+            [0, 5, 5, 0, 0, 0, -1, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, -1, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, -1, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, -1, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, -1, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        ]
+    )
+    expect(carrierP1.health).toBe(0);
+    expect(destroyerP1.health).toBe(2);
+    expect(player2.gameboard.checkShipsSunk()).toBe(false);
+});
+
+test("Be able to place ships on separate board for each player", () => {
+    const humanPlayer = new Player("Charlie");
+    const computerPlayer = new Player("Computer", true);
+    humanPlayer.createBattleship([1,1], "V");
+    computerPlayer.createCruiser([5,5], "H");
+    expect(humanPlayer.gameboard.board).toStrictEqual(
+        [
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 2, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 2, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 2, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 2, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        ]
+    )
+    expect(computerPlayer.gameboard.board).toStrictEqual(
+        [
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 5, 5, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        ]
+    )
+});
+
+test("Be able to attack each other's boards", () => {
+    const humanPlayer = new Player("Charlie");
+    const computerPlayer = new Player("Computer", true);
+    humanPlayer.createBattleship([1,1], "V");
+    computerPlayer.createCruiser([5,5], "H");
+    humanPlayer.attack([0,3], computerPlayer);
+    computerPlayer.attack([1,3], humanPlayer);
+    expect(humanPlayer.gameboard.board).toStrictEqual(
+        [
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 2, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 2, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, -1, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 2, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        ]
+    )
+    expect(computerPlayer.gameboard.board).toStrictEqual(
+        [
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [-1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 5, 5, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        ]
+    )
+    humanPlayer.attack([5,5], computerPlayer);
+    expect(computerPlayer.gameboard.ships[0].health).toBe(1)
+});
--- src/modules/player.ts
+++ src/modules/player.ts
@@ -2,20 +2,65 @@ import Ship from "../modules/ship";
 import Gameboard from "../modules/gameboard";
 
 class Player {
-
-    gameboard: Gameboard;
     name: string;
+    gameboard: Gameboard;
+    isComputer: boolean;
 
-    constructor(name: string) {
+    constructor(name: string, isComputer?: boolean) {
         this.name = name;
-        this.gameboard = new Gameboard;
+        this.isComputer = isComputer || false;
+        this.gameboard = new Gameboard();
     }
 
     attack(coordinates: number[], target: Player) {
         target.gameboard.receiveAttack(coordinates);
+        // Add condition if recieve null to redo
     }
 
-     
+    createCarrier(origin: [number, number], direction: string) {
+        const ship = new Ship(1, 5, origin, direction);
+        if (this.gameboard.placeShip(ship) === null) {
+            // loop until no longer an issue?
+        } else {
+            this.gameboard.placeShip(ship);
+        }
+    }
+
+    createBattleship(origin: [number, number], direction: string) {
+        const ship = new Ship(2, 4, origin, direction);
+        if (this.gameboard.placeShip(ship) === null) {
+            // loop until no longer an issue?
+        } else {
+            this.gameboard.placeShip(ship);
+        }
+    }
+
+    createSubmarine(origin: [number, number], direction: string) {
+        const ship = new Ship(3, 3, origin, direction);
+        if (this.gameboard.placeShip(ship) === null) {
+            // loop until no longer an issue?
+        } else {
+            this.gameboard.placeShip(ship);
+        }
+    }
+
+    createDestroyer(origin: [number, number], direction: string) {
+        const ship = new Ship(4, 3, origin, direction);
+        if (this.gameboard.placeShip(ship) === null) {
+            // loop until no longer an issue?
+        } else {
+            this.gameboard.placeShip(ship);
+        }
+    }
+
+    createCruiser(origin: [number, number], direction: string) {
+        const ship = new Ship(5, 2, origin, direction);
+        if (this.gameboard.placeShip(ship) === null) {
+            // loop until no longer an issue?
+        } else {
+            this.gameboard.placeShip(ship);
+        }
+    }
 }
 
 export default Player;
